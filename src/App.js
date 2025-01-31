02f8f2faa8a36080147eabf7b739b63a41604512
zain-ali-99 <zain.ali@maqsoodlabs.com>
2025-01-31 16:29:57 +0500

    one

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import React, { useState } from "react";
 import TestCompOne from "./components/TestCompOne";
 import TestCompTwo from "./components/TestCompTwo";
 
-/**
- * Main Component of the application.
- * Contains all the routing configurations
- *
- */
 export default function App() {
   const [selectedAccount, setSelectedAccount] = useState();
 
