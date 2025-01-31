dbdef0497fc18dfaabafb8affa629bf64518f3e1
zain-ali-99 <zain.ali@maqsoodlabs.com>
2025-01-31 16:16:24 +0500

    changes

--- src/components/TestCompOne.js
+++ src/components/TestCompOne.js
@@ -8,21 +8,21 @@ export default function TestCompOne() {
   //change
   return (
     <div className="bg-blue-800 p-6 text-white w-screen h-screen flex flex-col justify-center items-center">
-      <div className="flex flex-col items-center gap-4">
-        <h1 className="text-5xl font-bold">Test Component One</h1>
-        <p className="text-2xl">
+      <div className="flex flex-col items-center m-4">
+        <h1 className="text-5xl font-bold m-2">Test Component One</h1>
+        <p className="text-2xl m-2">
           This is a test component styled for TV app environments.
         </p>
-        <div className="flex w-full justify-around items-center gap-5">
+        <div className="flex w-full justify-around items-center m-4">
           <img
             src={Image3}
             alt="Placeholder One"
-            className="w-1/2 rounded-lg"
+            className="w-1/2 rounded-lg m-2"
           />
           <img
             src={Image4}
             alt="Placeholder Two"
-            className="w-1/2 rounded-lg"
+            className="w-1/2 rounded-lg m-2"
           />
         </div>
       </div>
