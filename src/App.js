6df90444a69a21582c5d8194ebfd5566153a68fe
zain-ali-99 <zain.ali@maqsoodlabs.com>
2025-01-31 15:18:19 +0500

    component change

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ export default function App() {
 
   return (
     <div>
-      {/* <TestCompOne /> */}
-      <TestCompTwo />
+      <TestCompOne />
+      {/* <TestCompTwo /> */}
     </div>
   );
 }
